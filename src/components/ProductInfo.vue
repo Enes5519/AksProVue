c623c60d6db63a94bd7d9a6bea0f5e3c79fcad40
Enes Yıldırım <enes5519@gmail.com>
2021-03-22 00:44:17 +0300

    Added product components

---
+++ src/components/ProductInfo.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="Product container">
+    <section class="Product-info">
+      <h1 class="title">{{ title }}</h1>
+      <hr />
+      <slot />
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'ProductInfo',
+  props: {
+    title: String,
+  },
+};
+</script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="Product container">
+    <aside class="Product-category" v-if="hasCategory">
+      <slot name="category" />
+    </aside>
+    <section class="Product-list">
+      <h1 class="title">{{ title }}</h1>
+      <hr />
+      <slot name="products" />
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'ProductList',
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+      hasCategory: false,
+    };
+  },
+  beforeMount() {
+    this.hasCategory = this.$slots.category !== undefined;
+  },
+};
+</script>
